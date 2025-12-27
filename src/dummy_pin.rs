f54272fcd06f4286620c1bb884e44f7b6cf8ef7f
Magnus Markling <magnus@markling.com>
2025-12-27 16:33:32 +0100

    got rid of pico-explorer

--- src/dummy_pin.rs
+++ src/dummy_pin.rs
@@ -13,3 +13,13 @@ impl embedded_hal::digital::OutputPin for DummyPin {
 impl embedded_hal::digital::ErrorType for DummyPin {
     type Error = core::convert::Infallible;
 }
+
+impl embedded_hal_02::digital::v2::OutputPin for DummyPin {
+    type Error = ();
+    fn set_high(&mut self) -> Result<(), Self::Error> {
+        Ok(())
+    }
+    fn set_low(&mut self) -> Result<(), Self::Error> {
+        Ok(())
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -26,7 +26,6 @@ use heapless::String;
 // use mousefood::prelude::*;
 // use mousefood::{EmbeddedBackendConfig, embedded_graphics::prelude::DrawTarget};
 use panic_probe as _;
-use pimoroni_pico_explorer::{PicoExplorer, Screen, all_pins};
 // use ratatui::{
 //     Frame, Terminal,
 //     style::{Style, Stylize},
@@ -35,14 +34,18 @@ use pimoroni_pico_explorer::{PicoExplorer, Screen, all_pins};
 use rp_pico::{
     Pins,
     hal::{
-        Adc, Clock, Sio, Spi, Watchdog,
+        self, Adc, Clock, Sio, Spi, Watchdog,
         clocks::init_clocks_and_plls,
         fugit::RateExtU32,
-        gpio::FunctionSpi,
+        gpio::{
+            self, FunctionSioOutput, FunctionSpi, Pin, PullNone,
+            bank0::{Gpio16, Gpio17, Gpio18, Gpio19},
+        },
         multicore::{Multicore, Stack},
         pwm::Slices,
+        spi::Enabled,
     },
-    pac::{CorePeripherals, Peripherals},
+    pac::{CorePeripherals, Peripherals, SPI0},
 };
 use st7789::ST7789;
 
@@ -69,10 +72,6 @@ fn main() -> ! {
     let cp = CorePeripherals::take().unwrap();
     let mut sio = Sio::new(p.SIO);
 
-    // let mut mc = Multicore::new(&mut pac.PSM, &mut pac.PPB, &mut sio.fifo);
-    // let cores = mc.cores();
-    // let core1 = &mut cores[1];
-
     let mut watchdog = Watchdog::new(p.WATCHDOG);
     let clocks = init_clocks_and_plls(
         XTAL_FREQ_HZ,
@@ -86,97 +85,16 @@ fn main() -> ! {
     .unwrap();
 
     let mut adc = Adc::new(p.ADC, &mut p.RESETS);
-    // let mut delay = Delay::new(core0.SYST, clocks.system_clock.freq().to_Hz());
     let mut delay = Delay::new(cp.SYST, clocks.system_clock.freq().to_Hz());
 
-    // let pins = Pins::new(p.IO_BANK0, p.PADS_BANK0, sio.gpio_bank0, &mut p.RESETS);
+    let pins = Pins::new(p.IO_BANK0, p.PADS_BANK0, sio.gpio_bank0, &mut p.RESETS);
 
-    debug!("pins set up");
+    let dc = pins.gpio16.reconfigure::<FunctionSioOutput, PullNone>();
+    let cs = pins.gpio17.reconfigure::<FunctionSioOutput, PullNone>();
+    let spi_sclk = pins.gpio18.reconfigure::<FunctionSpi, PullNone>();
+    let spi_mosi = pins.gpio19.reconfigure::<FunctionSpi, PullNone>();
 
-    //
-
-    // let mut mc = Multicore::new(&mut pac.PSM, &mut pac.PPB, &mut sio.fifo);
-    // let cores = mc.cores();
-    // let core1 = &mut cores[1];
-    // #[allow(static_mut_refs)]
-    // let _test = core1.spawn(unsafe { &mut CORE1_STACK.mem }, move || {
-    //     let mut core1 = unsafe { pac::CorePeripherals::steal() };
-    //     // let mut delay1 = Delay::new(core1.SYST, system_freq_hz);
-
-    //     let mut delay1 = Delay::new(core1.SYST, hz);
-
-    //     delay1.delay_us(500);
-
-    //     loop {
-    //         // debug!("From Core 1.");
-    //         delay1.delay_ms(500);
-    //     }
-    // });
-
-    //
-
-    // #[cfg(rp2040)]
-    // let mut timer = hal::Timer::new(pac.TIMER, &mut pac.RESETS, &clocks);
-
-    // #[cfg(rp2350)]
-    // let mut timer = hal::Timer::new_timer0(pac.TIMER0, &mut pac.RESETS, &clocks);
-
-    // let sio = hal::Sio::new(pac.SIO);
-
-    // let pins = hal::gpio::Pins::new(
-    //     pac.IO_BANK0,
-    //     pac.PADS_BANK0,
-    //     sio.gpio_bank0,
-    //     &mut pac.RESETS,
-    // );
-
-    // let mut led_pin_0 = pins.gpio25.into_push_pull_output();
-    // let mut led_pin_1 = pins.gpio18.into_push_pull_output();
-    // let mut led_pin_2 = pins.gpio19.into_push_pull_output();
-    // let mut led_pin_3 = pins.gpio20.into_push_pull_output();
-
-    // loop {
-    //     led_pin_0.set_high().unwrap();
-    //     timer.delay_ms(200);
-    //     led_pin_1.set_high().unwrap();
-    //     timer.delay_ms(200);
-    //     led_pin_3.set_low().unwrap();
-    //     timer.delay_ms(200);
-    //     led_pin_2.set_high().unwrap();
-    //     timer.delay_ms(200);
-    //     led_pin_1.set_low().unwrap();
-    //     timer.delay_ms(200);
-    //     led_pin_3.set_high().unwrap();
-    //     timer.delay_ms(200);
-    //     led_pin_2.set_low().unwrap();
-    //     timer.delay_ms(200);
-    // }
-
-    // let mut buf = ArrayString::<100>::new();
-
-    // writeln!(&mut buf, "Hello World!").unwrap();
-
-    // let buf: &str = "Hello world!";
-
-    // let (mut explorer, pins) = PicoExplorer::new(
-    //     p.IO_BANK0,
-    //     p.PADS_BANK0,
-    //     sio.gpio_bank0,
-    //     p.SPI0,
-    //     adc,
-    //     &mut p.RESETS,
-    //     &mut delay,
-    // );
-
-    let internal_pins =
-        all_pins::Pins::new(p.IO_BANK0, p.PADS_BANK0, sio.gpio_bank0, &mut p.RESETS);
-
-    let dc = internal_pins.spi_miso.reconfigure();
-    let cs = internal_pins.lcd_cs.reconfigure();
-    let spi_sclk = internal_pins.spi_sclk.reconfigure();
-    let spi_mosi = internal_pins.spi_mosi.reconfigure();
-
-    let spi_screen = Spi::new(p.SPI0, (spi_mosi, spi_sclk)).init(
+    let spi_screen = Spi::<_, _, _, 8>::new(p.SPI0, (spi_mosi, spi_sclk)).init(
         &mut p.RESETS,
         125u32.MHz(),
         16u32.MHz(),
@@ -185,9 +103,13 @@ fn main() -> ! {
 
     let spii_screen = SPIInterface::new(spi_screen, dc, cs);
 
-    let mut screen: Screen = ST7789::new(spii_screen, pimoroni_pico_explorer::DummyPin, 240, 240);
+    let mut screen = ST7789::new(spii_screen, DummyPin, 240, 240);
 
-    // // LCD pins (Pico Explorer)
+    screen.init(&mut delay).unwrap();
+    screen
+        .set_orientation(st7789::Orientation::Portrait)
+        .unwrap();
+
     // let dc = pins.gpio16.into_push_pull_output();
     // let cs = pins.gpio17.into_push_pull_output();
     // let sck = pins.gpio18.into_function::<FunctionSpi>();
@@ -229,86 +151,26 @@ fn main() -> ! {
     // let mut terminal = Terminal::new(backend).unwrap();
     // debug!("terminal initialized");
 
-    screen.clear(Rgb565::BLACK);
+    // screen.clear(Rgb565::BLACK);
 
-    loop {
-        debug!("drawing...");
+    debug!("drawing...");
 
-        {
-            // terminal.draw(draw).unwrap();
-        }
-
-        {
-            let style = MonoTextStyleBuilder::new()
-                .font(&FONT_10X20)
-                .text_color(Rgb565::GREEN)
-                .background_color(Rgb565::BLACK)
-                .build();
-            Text::with_alignment("Hello", Point::new(20, 30), style, Alignment::Left)
-                .draw(&mut screen)
-                .unwrap();
-        }
-
-        debug!("...done");
-        delay.delay_ms(10);
+    {
+        // terminal.draw(draw).unwrap();
     }
 
-    //
-
-    if false {
-        // let mut buzzer = pins.gpio4.into_push_pull_output();
-
-        // Rough ~1 kHz (500 µs high + 500 µs low)
-        loop {
-            // buzzer.set_high();
-            // delay.delay_us(500);
-            // buzzer.set_low();
-            // delay.delay_us(500);
-        }
-    } else {
-        let mut pwm_slices = Slices::new(p.PWM, &mut p.RESETS);
-        let pwm = &mut pwm_slices.pwm2;
-        pwm.enable();
-
-        // let freq_hz = 2000;
-        // let clk = 125_000_000;
-        // let top = clk / freq_hz;
-        // pwm0.set_top(top);
-
-        // 2 kHz: freq = clk_sys / (top + 1)
-        // with clk_sys = 125 MHz, top ≈ 62_499
-        // pwm0.set_top(62_499);
-        // pwm0.set_top(100_000);
-        // pwm0.enable();
-
-        let freq = 440;
-
-        let top = 125_000_000_u32 / freq / 250;
-        let top: u16 = top.try_into().unwrap();
-
-        let channel_a = &mut pwm.channel_a;
-        // let buzzer = pins.gpio0;
-        // let buzzer = pins.gpio4;
-        // channel_a.output_to(buzzer);
-
-        channel_a.set_duty(1100 / 8);
-
-        pwm.set_div_int(255);
-        pwm.set_div_frac(0);
-
-        loop {
-            // debug!("From Core 0.");
-            // delay.delay_ms(500);
-        }
-
-        loop {
-            // pwm.set_top(1100);
-            // delay.delay_ms(500);
-
-            // pwm.set_top(554);
-            // delay.delay_ms(500);
-        }
+    {
+        let style = MonoTextStyleBuilder::new()
+            .font(&FONT_10X20)
+            .text_color(Rgb565::GREEN)
+            .background_color(Rgb565::BLACK)
+            .build();
+        Text::with_alignment("Hello", Point::new(20, 30), style, Alignment::Left)
+            .draw(&mut screen)
+            .unwrap();
     }
+
+    debug!("...done");
 
     loop {}
 }
@@ -321,25 +183,6 @@ fn main() -> ! {
 //         .title("Mousefood");
 //     frame.render_widget(paragraph.block(bordered_block), frame.area());
 // }
-
-// fn set_freq<P: rp_pico::hal::pwm::SliceId>(pwm: &mut pwm::PwmSlice<P>, freq_hz: u32) {
-//     let clk = 125_000_000; // clk_sys
-//     let top = clk / freq_hz;
-//     pwm.set_top(top);
-// }
-
-// class Buzzer:
-//     def __init__(self, pin):
-//         self.pwm = PWM(Pin(pin))
-
-//     def set_tone(self, freq, duty=0.5):
-//         if freq < 50.0:  # uh... https://github.com/micropython/micropython/blob/af64c2ddbd758ab6bac0fcca94c66d89046663be/ports/rp2/machine_pwm.c#L105-L119
-//             self.pwm.duty_u16(0)
-//             return False
-
-//         self.pwm.freq(freq)
-//         self.pwm.duty_u16(int(65535 * duty))
-//         return True
 
 #[unsafe(link_section = ".bi_entries")]
 #[used]
