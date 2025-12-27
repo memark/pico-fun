32e84b68db9f506fd7f2a58e8effee03683d9953
Magnus Markling <magnus@markling.com>
2025-12-27 18:16:55 +0100

    showing chart + cleanup

--- src/main.rs
+++ src/main.rs
@@ -21,12 +21,11 @@ use embedded_graphics::{
 };
 use embedded_hal::{delay::DelayNs, spi::MODE_0};
 // use embedded_hal_bus::spi::ExclusiveDevice;
-use heapless::String;
+use heapless::{String, Vec};
 use mousefood::{EmbeddedBackend, EmbeddedBackendConfig};
 // use mipidsi::{Builder, interface::SpiInterface, models, options::ColorInversion};
-// use mousefood::prelude::*;
-// use mousefood::{EmbeddedBackendConfig, embedded_graphics::prelude::DrawTarget};
 use panic_probe as _;
+use ratatui::widgets::Borders;
 use rp_pico::{
     Pins,
     hal::{
@@ -47,7 +46,7 @@ use st7789::ST7789;
 
 const XTAL_FREQ_HZ: u32 = 12_000_000_u32;
 
-static mut CORE1_STACK: Stack<4096> = Stack::new();
+// static mut CORE1_STACK: Stack<4096> = Stack::new();
 
 #[global_allocator]
 static HEAP: Heap = Heap::empty();
@@ -59,7 +58,7 @@ fn core1_task() {
 #[entry]
 fn main() -> ! {
     unsafe {
-        embedded_alloc::init!(HEAP, 100 * 1024);
+        embedded_alloc::init!(HEAP, 120 * 1024);
     }
 
     info!("Application started");
@@ -98,54 +97,16 @@ fn main() -> ! {
     let cs = pins.gpio17.reconfigure::<FunctionSioOutput, PullNone>();
     let spii_screen = SPIInterface::new(spi_screen, dc, cs);
 
-    let mut screen = ST7789::new(spii_screen, Some(DummyPin), Some(DummyPin), 240, 240);
+    let mut screen = ST7789::new(spii_screen, None::<DummyPin>, None::<DummyPin>, 240, 240);
     screen.init(&mut delay).unwrap();
     screen
         .set_orientation(st7789::Orientation::Portrait)
         .unwrap();
     screen.clear(Rgb565::BLACK);
 
-    // let dc = pins.gpio16.into_push_pull_output();
-    // let cs = pins.gpio17.into_push_pull_output();
-    // let sck = pins.gpio18.into_function::<FunctionSpi>();
-    // let mosi = pins.gpio19.into_function::<FunctionSpi>();
-    // let miso = pins.gpio4.into_function::<FunctionSpi>(); // dummy, not wired
-    // let rst = pins.gpio20.into_push_pull_output(); // not wired -> acts as "no reset"
-
-    // // SPI0 in MODE_0 is what ST7789 expects
-    // let spi = Spi::<_, _, _>::new(p.SPI0, (mosi, miso, sck)).init(
-    //     &mut p.RESETS,
-    //     clocks.peripheral_clock.freq(),
-    //     32.MHz(),
-    //     embedded_hal::spi::MODE_0,
-    // );
-
-    // debug!("spi initialized");
-
-    // static mut BUF: [u8; 1024] = [0; 1024];
-
-    // #[allow(static_mut_refs)]
-    // let buffer = unsafe { &mut BUF };
-
-    // // let spi = Spi::new(Bus::Spi0, SlaveSelect::Ss1, 60_000_000_u32, Mode::Mode0).unwrap();
-    // let spi_device = ExclusiveDevice::new_no_delay(spi, DummyPin).unwrap();
-    // // let mut buffer = [0_u8; 512];
-    // let di = SpiInterface::new(spi_device, dc, buffer);
-    // // let mut delay = Delay::new();
-    // let mut display = Builder::new(models::ST7789, di)
-    //     .display_size(240, 240)
-    //     .invert_colors(ColorInversion::Inverted)
-    //     .init(&mut delay)
-    //     .unwrap();
-
-    // debug!("display initialized");
-
     {
-        use ratatui::{
-            Frame, Terminal,
-            style::{Style, Stylize as _},
-            widgets::{Block, Paragraph, Wrap},
-        };
+        use heapless::Vec;
+        use ratatui::{prelude::*, symbols::*, widgets::*};
 
         let backend = EmbeddedBackend::new(&mut screen, EmbeddedBackendConfig::default());
         debug!("backend initialized");
@@ -153,35 +114,79 @@ fn main() -> ! {
         let mut terminal = Terminal::new(backend).unwrap();
         debug!("terminal initialized");
 
-        fn draw(frame: &mut Frame) {
-            let text = "Ratatui on embedded devices!";
-            let paragraph = Paragraph::new(text.dark_gray()).wrap(Wrap { trim: true });
-            let bordered_block = Block::bordered()
-                .border_style(Style::new().yellow())
-                .title("Mousefood");
-            frame.render_widget(paragraph.block(bordered_block), frame.area());
-        }
-
         debug!("drawing...");
 
-        terminal.draw(draw).unwrap();
-    }
+        terminal
+            .draw(|frame| {
+                // let text = "Ratatui on Raspberry Pi Pico!";
+                // let paragraph = Paragraph::new(text.blue().bold())
+                //     .wrap(Wrap { trim: true })
+                //     .centered();
+                // let bordered_block = Block::bordered()
+                //     .padding(Padding::uniform(2))
+                //     .border_style(Style::new().yellow().bold())
+                //     .title("Mousefood");
+                // frame.render_widget(paragraph.block(bordered_block), frame.area());
 
-    {
-        // let style = MonoTextStyleBuilder::new()
-        //     .font(&FONT_10X20)
-        //     .text_color(Rgb565::GREEN)
-        //     .background_color(Rgb565::BLACK)
-        //     .build();
-        // Text::with_alignment("Hello", Point::new(20, 30), style, Alignment::Left)
-        //     .draw(&mut screen)
-        //     .unwrap();
-        // Text::with_alignment("World", Point::new(20, 50), style, Alignment::Left)
-        //     .draw(&mut screen)
-        //     .unwrap();
-    }
+                // let line_gauge = LineGauge::default()
+                //     .block(Block::bordered().title("Progress"))
+                //     .filled_style(Style::new().white().on_black().bold())
+                //     .filled_symbol(symbols::line::THICK_HORIZONTAL)
+                //     .ratio(0.4);
+                // frame.render_widget(line_gauge, frame.area());
 
-    debug!("...done");
+                let datasets = [
+                    Dataset::default()
+                        .name("Heavy")
+                        .marker(Marker::Dot)
+                        .graph_type(GraphType::Scatter)
+                        .style(Style::new().yellow())
+                        .data(&HEAVY_PAYLOAD_DATA),
+                    Dataset::default()
+                        .name("Medium".underlined())
+                        .marker(Marker::Braille)
+                        .graph_type(GraphType::Scatter)
+                        .style(Style::new().magenta())
+                        .data(&MEDIUM_PAYLOAD_DATA),
+                    Dataset::default()
+                        .name("Small")
+                        .marker(Marker::Dot)
+                        .graph_type(GraphType::Scatter)
+                        .style(Style::new().cyan())
+                        .data(&SMALL_PAYLOAD_DATA),
+                ];
+
+                let chart = Chart::new(datasets.to_vec())
+                    .block(
+                        Block::bordered().title(
+                            Line::from("Mousefood - Ratatui on Pi Pico RP2040")
+                                .cyan()
+                                .bold()
+                                .centered(),
+                        ),
+                    )
+                    .x_axis(
+                        Axis::default()
+                            .title("Year")
+                            .bounds([1960., 2020.])
+                            .style(Style::default().fg(Color::Gray))
+                            .labels(["1960", "1990", "2020"]),
+                    )
+                    .y_axis(
+                        Axis::default()
+                            .title("Cost")
+                            .bounds([0., 75000.])
+                            .style(Style::default().fg(Color::Gray))
+                            .labels(["0", "37 500", "75 000"]),
+                    )
+                    .hidden_legend_constraints((Constraint::Ratio(1, 2), Constraint::Ratio(1, 2)));
+
+                frame.render_widget(chart, frame.area());
+            })
+            .unwrap();
+
+        debug!("...done");
+    }
 
     loop {}
 }
@@ -194,4 +199,74 @@ pub static PICOTOOL_ENTRIES: [rp_binary_info::EntryAddr; 5] = [
     rp_binary_info::rp_program_description!(c"Blinky Example"),
     rp_binary_info::rp_cargo_homepage_url!(),
     rp_binary_info::rp_program_build_attribute!(),
+];
+
+const HEAVY_PAYLOAD_DATA: [(f64, f64); 9] = [
+    (1965., 8200.),
+    (1967., 5400.),
+    (1981., 65400.),
+    (1989., 30800.),
+    (1997., 10200.),
+    (2004., 11600.),
+    (2014., 4500.),
+    (2016., 7900.),
+    (2018., 1500.),
+];
+
+const MEDIUM_PAYLOAD_DATA: [(f64, f64); 29] = [
+    (1963., 29500.),
+    (1964., 30600.),
+    (1965., 177_900.),
+    (1965., 21000.),
+    (1966., 17900.),
+    (1966., 8400.),
+    (1975., 17500.),
+    (1982., 8300.),
+    (1985., 5100.),
+    (1988., 18300.),
+    (1990., 38800.),
+    (1990., 9900.),
+    (1991., 18700.),
+    (1992., 9100.),
+    (1994., 10500.),
+    (1994., 8500.),
+    (1994., 8700.),
+    (1997., 6200.),
+    (1999., 18000.),
+    (1999., 7600.),
+    (1999., 8900.),
+    (1999., 9600.),
+    (2000., 16000.),
+    (2001., 10000.),
+    (2002., 10400.),
+    (2002., 8100.),
+    (2010., 2600.),
+    (2013., 13600.),
+    (2017., 8000.),
+];
+
+const SMALL_PAYLOAD_DATA: [(f64, f64); 23] = [
+    (1961., 118_500.),
+    (1962., 14900.),
+    (1975., 21400.),
+    (1980., 32800.),
+    (1988., 31100.),
+    (1990., 41100.),
+    (1993., 23600.),
+    (1994., 20600.),
+    (1994., 34600.),
+    (1996., 50600.),
+    (1997., 19200.),
+    (1997., 45800.),
+    (1998., 19100.),
+    (2000., 73100.),
+    (2003., 11200.),
+    (2008., 12600.),
+    (2010., 30500.),
+    (2012., 20000.),
+    (2013., 10600.),
+    (2013., 34500.),
+    (2015., 10600.),
+    (2018., 23100.),
+    (2019., 17300.),
 ];
